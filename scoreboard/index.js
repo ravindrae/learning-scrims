0a4357c2b946087c1326b64098c48af2521f2b4e
Ravindra Eedupuganti <ravindraleader@gmail.com>
2025-04-11 12:02:06 +0530

    score board scrim added

---
+++ scoreboard/index.js
@@ -0,0 +1,32 @@
+let homeScore = 0
+let guestScore = 0
+
+function incrementHomeScoreOne() {
+    homeScore += 1
+    document.getElementById('home-score').textContent = homeScore
+}
+
+function incrementHomeScoreTwo() {
+    homeScore += 2
+    document.getElementById('home-score').textContent = homeScore
+}
+
+function incrementHomeScoreThree() {
+    homeScore += 3
+    document.getElementById('home-score').textContent = homeScore
+}
+
+function incrementGuestScoreOne() {
+    guestScore += 1
+    document.getElementById('guest-score').textContent = guestScore
+}
+
+function incrementGuestScoreTwo() {
+    guestScore += 2
+    document.getElementById('guest-score').textContent = guestScore
+}
+
+function incrementGuestScoreThree() {
+    guestScore += 3
+    document.getElementById('guest-score').textContent = guestScore
+}
